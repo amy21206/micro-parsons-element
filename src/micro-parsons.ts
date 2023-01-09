cd0ddc27338c4b066b9e1077ba85dad6fe00346d
Zihan Wu <zihanwu.amy@gmail.com>
2023-01-09 19:21:51 +0800

    :recycle: Moving css to a separate file

--- src/micro-parsons.ts
+++ src/micro-parsons.ts
@@ -1,5 +1,6 @@
 import { MicroParsonsEvent } from './LoggingEvents';
 import { ParsonsInput } from './ParsonsInput';
+import './style/style.css';
 
 export class HParsonsElement extends HTMLElement {
 
@@ -26,7 +27,6 @@ export class HParsonsElement extends HTMLElement {
 
         this.root = this;
 
-        this.addStyle();
         const reusable = this.getAttribute('reuse-blocks') ? true : false;
         const randomize = this.getAttribute('randomize') ? true : false;
         this.hparsonsInput = new ParsonsInput(this, reusable, randomize);
@@ -51,29 +51,6 @@ export class HParsonsElement extends HTMLElement {
 
     get parsonsData(): Array<string> {
         return this._parsonsData;
-    }
-
-    // TODO[refactor]: put stylesheet in a separate css/scss file
-    private addStyle = (): void => {
-        const sheet = document.createElement('style');
-        // parsons block
-        sheet.innerHTML += '.hparsons-input {padding: 15px;}\n';
-        sheet.innerHTML += '.hparsons-tip { font-style: italic; }\n';
-        sheet.innerHTML += '.parsons-tooltip{ visibility: hidden; max-width: 120px; width: max-content; background-color: black; color: #fff; text-align: center; border-radius: 6px; padding: 5px; position: absolute; z-index: 1; top: 30px;}\n';
-        sheet.innerHTML += '.parsons-block:hover .parsons-tooltip{ visibility: visible;}\n';
-        sheet.innerHTML += '.parsons-block {display: inline-block; font-family: monospace; border-color:gray; margin: 0 1px; position: relative; border-radius: 10px; background-color: #efefef; border: 1px solid #d3d3d3; padding: 5px 10px; margin-top: 5px;}\n';
-        sheet.innerHTML += '.drop-area .parsons-block.incorrectPosition {background-color: #ffbaba; border: 1px solid red;}\n';
-        sheet.innerHTML += '.parsons-block:hover, .parsons-block:focus { border-color: black;}\n';
-        sheet.innerHTML += '.drop-area { background-color: #ffa; padding: 0 5px 5px 5px; min-height: 32px; height: auto; margin: 2px 0;}\n';
-        sheet.innerHTML += '.drop-area.incorrect { background-color: #f2dede; border-color: #f2b6b6}\n';
-        sheet.innerHTML += '.drop-area.correct { background-color: #dff0d8; border-color: #ade595}\n';
-        // TODO:(UI) move the tooltip to the top of the line
-        // sheet.innerHTML += '.parsons-block .tooltip { visibility: hidden; width: 200px;  background-color: black; color: #fff; text-align: center; padding: 5px; border-radius: 6px;  position: absolute; z-index: 1; margin: 0 10px; bottom: 120%; margin-left: -100px;}\n';
-        // sheet.innerHTML += '.parsons-block .tooltip::after {content: " ";position: absolute; top: 100%;left: 50%; margin-left: -5px; border-width: 5px; border-style: solid; border-color: black transparent transparent transparent;}\n';
-        sheet.innerHTML += '.drag-area .parsons-block:hover .tooltip { visibility: visible;}\n';
-        sheet.innerHTML += '.drag-area { background-color: #efefff; padding: 0 5px 5px 5px; min-height: 32px; height: auto; margin: 2px 0; }\n';
-
-        this.root.appendChild(sheet);
     }
 
     public logEvent = (eventContent: any): void => {
