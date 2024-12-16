3ea2233a1c2cf8ce22bddcf8a33d1ac672959e86
Zihan Wu <zihanwu.amy@gmail.com>
2024-12-16 14:39:22 -0500

    Adding documentations to event types

--- src/LoggingEvents.ts
+++ src/LoggingEvents.ts
@@ -1,3 +1,4 @@
+// This file contains the event definition for micro Parsons problems.
 export namespace MicroParsonsEvent {
 
     export enum InputAction {
@@ -9,7 +10,15 @@ export namespace MicroParsonsEvent {
     export type Input = {
         'type': 'input';
         action: InputAction;
-        position: [number, number];
+        // Position: The index of the block before the input action, and index of the block after the input action.
+        // -1 indicates that the block is not in the answer.
+        // e.g. [-1, 0] Means a block is *added* to the answer to the *beginning*;
+        //      [2, -1] means a block at index 2 is *removed* from the answer;
+        //      [1, 2] means the moved block previously had an index of 1. After moving it, its index becomes 2.
+        // * Note1: I recognize it is not ideal to store in a list, but tuples are not defined in JSON.
+        // * Note2: This input event does not capture the movement in the "source" blocks.
+        position: [number, number]; 
+        // The final answer *after* the input action.
         answer: Array<string>;
     }
 
